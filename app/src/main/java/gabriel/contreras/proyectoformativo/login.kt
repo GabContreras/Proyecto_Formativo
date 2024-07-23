0173fd5f93b6c5d8efb81fbc151e4adb30d79fcd
GabContreras <contrerasgab01@gmail.com>
2024-07-23 10:37:55 -0600

    Cambios asdmklasndbasocdsac

--- app/src/main/java/gabriel/contreras/proyectoformativo/login.kt
+++ app/src/main/java/gabriel/contreras/proyectoformativo/login.kt
@@ -1,10 +1,19 @@
 package gabriel.contreras.proyectoformativo
 
+import Modelo.ClaseConexion
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import kotlinx.coroutines.launch
+
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +25,39 @@ class login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val txtUsuario = findViewById<EditText>(R.id.txtUsuarioRegistro)
+        val txtContrasena = findViewById<EditText>(R.id.txtContrasenaRegistro)
+        val btnIngresar = findViewById<ImageView>(R.id.btnIngresar)
+        val txtRegistrare = findViewById<TextView>(R.id.btnRegistrarse)
+
+
+        //Botones para ingresar al sistema
+
+        btnIngresar.setOnClickListener {
+            val pantallaPrincipal = Intent(this, lista_pacientes::class.java)
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val objConexion = ClaseConexion().cadenaConexion()
+
+
+                val comprobacionUsuario = objConexion?.prepareStatement("SELECT * FROM TB_USUARIO WHERE NOMBRE_DE_USUARIO = ? AND CONTRASENA = ?")!!
+                comprobacionUsuario.setString(1, txtUsuario.text.toString())
+                comprobacionUsuario.setString(2, txtContrasena.text.toString())
+                val resultado = comprobacionUsuario.executeQuery()
+
+                if (resultado.next()) {
+                    startActivity(pantallaPrincipal)
+                } else {
+                    println("Usuario no encontrado, verifique las credenciales")
+                }
+            }
+        }
+        txtRegistrare.setOnClickListener {
+            //Cambio de pantalla para poder registrarse
+            val pantallaRegistrarse = Intent(this, registro_usuario::class.java)
+            startActivity(pantallaRegistrarse)
+        }
+
     }
 }
--- app/src/main/java/gabriel/contreras/proyectoformativo/registro_usuario.kt
+++ app/src/main/java/gabriel/contreras/proyectoformativo/registro_usuario.kt
@@ -1,10 +1,19 @@
 package gabriel.contreras.proyectoformativo
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import Modelo.ClaseConexion
+import android.widget.Toast
+
 
 class registro_usuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +25,33 @@ class registro_usuario : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val txtUsuarioRegistrarse = findViewById<EditText>(R.id.txtUsuarioRegistro)
+        val txtContrasenaRegistrarse = findViewById<EditText>(R.id.txtContrasenaRegistro)
+        val btnRegistrarse = findViewById<ImageView>(R.id.btnCrearCuenta)
+
+
+
+        btnRegistrarse.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+
+                val objConexion = ClaseConexion().cadenaConexion()
+
+
+                val crearUsuario =
+                    objConexion?.prepareStatement("INSERT INTO Enfermera(usuario, contrasena) VALUES (?, ?) ")!!
+                crearUsuario.setString(2, txtUsuarioRegistrarse.text.toString())
+                crearUsuario.setString(3, txtContrasenaRegistrarse.text.toString())
+                crearUsuario.executeUpdate()
+                withContext(Dispatchers.Main) {
+
+                    Toast.makeText(this@registro_usuario, "Usuario creado, Regresa a la pantalla anterior para poder iniciar sesión en el sistema", Toast.LENGTH_LONG).show()
+                    txtUsuarioRegistrarse.setText("")
+                    txtContrasenaRegistrarse.setText("")
+                }
+            }
+
+        }
+
+
     }
 }
